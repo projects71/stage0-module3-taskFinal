7ec86f40761cdf4b9ee990250abf191889ae7a1a
Khusan <khusanabdukarimov007@gmail.com>
2023-05-08 18:03:31 +0500

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int sum = 0;
+
+        while (number != 0) {
+            sum = sum * 10 + number % 10;
+            number /= 10;
+        }
+
+        System.out.println(sum);
     }
 }
